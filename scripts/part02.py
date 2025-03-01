e5b3c4c3b9d357eb2641b62084ff50195822bbfd
Mateus Abdallah <abdallahmateus@gmail.com>
2025-03-01 15:06:57 -0800

    add indicators and doing predictions

--- scripts/part02.py
+++ scripts/part02.py
@@ -6,8 +6,6 @@ import pandas as pd
 df = pd.read_csv('../data/all_stocks_5yr.csv')
 
 # %%
-
-# %%
 # Simple Moving Average
 df['SMA_20'] = df.groupby('name')['close'].transform(lambda x: x.rolling(window=20).mean())
 
@@ -16,21 +14,25 @@ df['SMA_20'] = df.groupby('name')['close'].transform(lambda x: x.rolling(window=
 df['EMA_20'] = df.groupby('name')['close'].transform(lambda x: x.ewm(span=20, adjust=False).mean())
 
 # %%
+# Relative Strength Index
 
+df['price_change'] = df.groupby('name')['close'].diff()
+df['gain'] = df['price_change'].apply(lambda x: x if x > 0 else 0)
+df['loss'] = df['price_change'].apply(lambda x: -x if x < 0 else 0)
+
+df['avg_gain'] = df.groupby('name')['gain'].transform(lambda x: x.rolling(window=20).mean())
+df['avg_loss'] = df.groupby('name')['loss'].transform(lambda x: x.rolling(window=20).mean())
+
+
+df['rs'] = df['avg_gain'] / df['avg_loss']
+df['rsi'] = 100 - (100 / (1 + df['rs']))
+
+# %%
+# On-Balance Volume
+df['obv'] = df.groupby('name').apply(lambda x: x['volume'] * (1 if x['close'].diff().iloc[-1] > 0 else -1)).reset_index(level=0, drop=True)
+
+df['obv'] = df.groupby('name')['obv'].cumsum()
 
 # %%
 
-df_filter_ko = df['name'] == "KO"
-df[df_filter_ko]
-
-# %%
-
-ko_14 = df[df_filter_ko]
-ko_14[:30]
-
- # %%
-#Simple Moving Average
-ko_14['SMA'] = ko_14['close'].mean()
-
-# appl_max = appl_14['close'].max()
-# appl_min = appl_14['close'].min()
+df = df.rename(columns={'SMA_20':'sma_20', 'EMA_20':'ema_20'})
