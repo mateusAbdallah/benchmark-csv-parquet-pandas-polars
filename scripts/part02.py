294ab651044fb83a99e73afcbf680c276bc65fac
Mateus Abdallah <abdallahmateus@gmail.com>
2025-03-01 11:39:18 -0800

    creating new columns analizing the close price

--- scripts/part02.py
+++ scripts/part02.py
@@ -7,13 +7,26 @@ df = pd.read_csv('../data/all_stocks_5yr.csv')
 
 # %%
 
+# %%
+# Simple Moving Average
+df['SMA_20'] = df.groupby('name')['close'].transform(lambda x: x.rolling(window=20).mean())
+
+# %%
+# Exponential Moving Average
+df['EMA_20'] = df.groupby('name')['close'].transform(lambda x: x.ewm(span=20, adjust=False).mean())
+
+# %%
+
+
+# %%
+
 df_filter_ko = df['name'] == "KO"
 df[df_filter_ko]
 
 # %%
 
 ko_14 = df[df_filter_ko]
-ko_14
+ko_14[:30]
 
  # %%
 #Simple Moving Average
