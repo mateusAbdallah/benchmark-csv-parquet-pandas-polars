45d17ac88f7860fa7c56139acd38852438199b4d
Mateus Abdallah <abdallahmateus@gmail.com>
2025-03-08 13:21:23 -0800

    selecting variables to do the regression

--- scripts/part02.py
+++ scripts/part02.py
@@ -1,13 +1,15 @@
 # %%
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 
+
 # %%
 
 df = pd.read_csv('../data/all_stocks_5yr.csv')
-
+df
 # %%
 # Simple Moving Average
 df['SMA_20'] = df.groupby('name')['close'].transform(lambda x: x.rolling(window=20).mean())
@@ -39,30 +41,28 @@ df['obv'] = df.groupby('name')['obv'].cumsum()
 # %%
 # Rename columns
 df = df.rename(columns={'SMA_20':'sma_20', 'EMA_20':'ema_20'})
-# Drop null values
-df = df.dropna()
+# %%
+df['prev_close'] = df.groupby('name')['close'].shift(1)
+df = df.dropna()  # Drop NaN values from shifting
 
+
+# %% 
+
+# Train-test split (80-20)
+train, test = train_test_split(df, test_size=0.2, shuffle=False)
+
+# Features and Target
+X_train, X_test = train[['prev_close']], test[['prev_close']]
+y_train, y_test = train['close'], test['close']
 # %%
 
-features = df.drop(columns=['date', 'close', 'name']) 
-target = df['close']
-
-# %%
-
-X_train, X_test, y_train, y_test = train_test_split(features, target, random_state=10, test_size=0.2)
-
-# %%
-
-# Linear Regression
-
+# Linear Regression Model
 lr_model = LinearRegression()
 lr_model.fit(X_train, y_train)
 lr_pred = lr_model.predict(X_test)
-
 # %%
 
 # Random Forest Model
-
-rf_model = RandomForestRegressor(n_estimators=10, random_state=10)
+rf_model = RandomForestRegressor(n_estimators=50, random_state=42)
 rf_model.fit(X_train, y_train)
 rf_pred = rf_model.predict(X_test)
